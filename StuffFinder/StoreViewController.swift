f098902c6d05316482b6002b9c4b0da59c5660b0
ArpitLokwani <arpit.lokwani@gmail.com>
2016-12-21 19:31:11 +0530

    CoreData integrated

--- StuffFinder/StoreViewController.swift
+++ StuffFinder/StoreViewController.swift
@@ -7,20 +7,67 @@
 //
 
 import UIKit
+import CoreData
 
-class StoreViewController: UIViewController {
+class StoreViewController: UIViewController,UITextFieldDelegate {
+
+    @IBOutlet weak var stuffNameTextFiel: UITextField!
+    
+    @IBOutlet weak var locationTextViewField: UITextView!
+    var stuffDetail = [NSManagedObject]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        stuffNameTextFiel.delegate = self
+       
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func recorderButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func cameraButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func storeButtonPressed(_ sender: Any) {
+        
+        let appDelegate =
+            UIApplication.shared.delegate as! AppDelegate
+        
+        let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        //2
+        let entity =  NSEntityDescription.entity(forEntityName: "StuffDetail",
+                                                 in:managedContext)
+        
+        let person = NSManagedObject(entity: entity!,
+                                     insertInto: managedContext)
+        
+        person.setValue(stuffNameTextFiel.text, forKey: "name")
+        person.setValue(locationTextViewField.text, forKey: "location")
+        
+        
+        do {
+            try managedContext.save()
+            self.navigationController?.popViewController(animated: true)
+            
+            //5
+        } catch let error as NSError  {
+            print("Could not save \(error), \(error.userInfo)")
+        }
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+     public func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        stuffNameTextFiel.resignFirstResponder()
+        return true
+        
+    }
 
     /*
     // MARK: - Navigation
--- StuffFinder/ViewController.swift
+++ StuffFinder/ViewController.swift
@@ -7,18 +7,44 @@
 //
 
 import UIKit
-
+import CoreData
 class ViewController: UIViewController {
 
-    
+    var stuffDetails = [NSManagedObject]()
     @IBAction func storeMyStuffButtonPressed(_ sender: Any) {
         let next = self.storyboard?.instantiateViewController(withIdentifier: "StoreViewController") as! StoreViewController
         self.navigationController?.pushViewController(next, animated: true)
     }
     
     @IBAction func findMyStuffButtonPressed(_ sender: Any) {
-        let next = self.storyboard?.instantiateViewController(withIdentifier: "FinderViewController") as! FinderViewController
-        self.navigationController?.pushViewController(next, animated: true)
+       // let next = self.storyboard?.instantiateViewController(withIdentifier: "FinderViewController") as! FinderViewController
+        //self.navigationController?.pushViewController(next, animated: true)
+        //1
+        let appDelegate =
+            UIApplication.shared.delegate as! AppDelegate
+        
+        let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        //2
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "StuffDetail")
+        
+        //3
+        do {
+           let results =
+             try managedContext.fetch(fetchRequest)
+            
+          stuffDetails = results as! [NSManagedObject]
+            
+         let stuff = stuffDetails[0]
+            
+          print(stuff.value(forKey: "name"))
+            
+           
+            // let person
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+        
     }
     
     
